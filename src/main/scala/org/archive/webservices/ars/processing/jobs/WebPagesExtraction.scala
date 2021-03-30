cf104f4456b500d7f0647417c7892080a87401a3
Nick Ruest <ruestn@gmail.com>
2021-03-30 09:17:31 -0400

    Add MIME type distribution chart to webpages derivative. (#47)
    - Resolves #46

--- src/main/scala/org/archive/webservices/ars/processing/jobs/WebPagesExtraction.scala
+++ src/main/scala/org/archive/webservices/ars/processing/jobs/WebPagesExtraction.scala
@@ -6,11 +6,13 @@ import io.archivesunleashed.ArchiveRecord
 import io.archivesunleashed.app.WebPagesExtractor
 import org.apache.spark.rdd.RDD
 import org.archive.webservices.ars.model.ArsCloudJobCategories
-import org.archive.webservices.ars.processing.jobs.shared.AutJob
+import org.archive.webservices.ars.processing.jobs.shared.BinaryInformationAutJob
 
-object WebPagesExtraction extends AutJob {
+object WebPagesExtraction extends BinaryInformationAutJob {
   val name = "Extract webpages"
-  val category = ArsCloudJobCategories.Text
+
+  override val category = ArsCloudJobCategories.Text
+
   val description =
     "Create a CSV with the following columns: crawl date, web domain, URL, MIME type as provided by the web server, MIME type as detected by Apache TIKA, and content (HTTP headers and HTML removed)."
 
