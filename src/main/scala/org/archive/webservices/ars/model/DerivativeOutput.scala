266975087da0acc511f8946e8e1bd348fdab7ba8
Helge Holzmann <helge@archive.org>
2021-05-28 03:24:08 +0200

    multiple small updates for production (#64)

--- src/main/scala/org/archive/webservices/ars/model/DerivativeOutput.scala
+++ src/main/scala/org/archive/webservices/ars/model/DerivativeOutput.scala
@@ -4,18 +4,34 @@ import java.time.Instant
 
 import org.apache.hadoop.fs.Path
 import org.archive.helge.sparkling.io.HdfsIO
+import org.archive.helge.sparkling.util.StringUtil
 import org.archive.webservices.ars.io.IOHelper
 
+import scala.util.Try
+
 case class DerivativeOutput(filename: String, dir: String, mimeType: String) {
+  import DerivativeOutput._
+
   lazy val path: String = dir + "/" + filename
+
   lazy val (size, time) = {
     val status = HdfsIO.fs.getFileStatus(new Path(path))
     (status.getLen, status.getModificationTime)
   }
   lazy val sizeStr: String = IOHelper.sizeStr(path)
 
-  lazy val lineCount: String = (HdfsIO.countLines(path) - 1).toString
+  lazy val lineCount: Long =
+    if (HdfsIO.exists(path + lineCountFileSuffix))
+      Try(HdfsIO.lines(path + lineCountFileSuffix).head.toLong).getOrElse(-1)
+    else -1
+
+  lazy val lineCountStr: Option[String] =
+    if (lineCount < 0) None else Some(StringUtil.formatNumber(lineCount, 0))
 
   lazy val timeStr: String =
     Instant.ofEpochMilli(time).toString.stripSuffix("Z").replace("T", " ")
+}
+
+object DerivativeOutput {
+  val lineCountFileSuffix = "_linecount"
 }
