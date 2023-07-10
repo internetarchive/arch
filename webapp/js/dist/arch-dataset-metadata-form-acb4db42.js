a2b3d9797aa0ac2f6976c0b27e1e382136604f74
Derek Enos <derek.enos@gmail.com>
2023-07-10 14:46:39 -0400

    chore: rebundled web components

---
+++ webapp/js/dist/arch-dataset-metadata-form-acb4db42.js
@@ -0,0 +1,2 @@
+import"./chunk-styles-75502ec5.js";export{A as ArchDatasetMetadataForm}from"./chunk-arch-dataset-metadata-form-8b222489.js";import"./chunk-query-all-273a2103.js";
+//# sourceMappingURL=arch-dataset-metadata-form-acb4db42.js.map
