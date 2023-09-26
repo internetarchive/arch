ba6b1cc72985cccab7e59206df8ef90ddb1b6457
Derek Enos <derek.enos@gmail.com>
2023-09-26 10:57:59 -0400

    fix: increase maxLength of description dataset metadata field to 1k chars per request from Karl

---
+++ webapp/js/dist/arch-dataset-metadata-form-2c7a1218.js
@@ -0,0 +1,2 @@
+import"./chunk-styles-bfd25595.js";export{A as ArchDatasetMetadataForm}from"./chunk-arch-dataset-metadata-form-5d9718fe.js";import"./chunk-query-all-fe6f60e4.js";
+//# sourceMappingURL=arch-dataset-metadata-form-2c7a1218.js.map
