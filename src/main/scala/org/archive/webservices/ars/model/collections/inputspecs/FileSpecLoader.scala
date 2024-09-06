f45629b38f9fa1b56c0ee0420ca6a616d8f23135
Helge Holzmann <helge@archive.org>
2024-09-06 11:30:47 +0200

    move petaboxFiles method over to the ARCH petabox plugin repo

--- src/main/scala/org/archive/webservices/ars/model/collections/inputspecs/FileSpecLoader.scala
+++ src/main/scala/org/archive/webservices/ars/model/collections/inputspecs/FileSpecLoader.scala
@@ -1,14 +1,10 @@
 package org.archive.webservices.ars.model.collections.inputspecs
 
-import _root_.io.circe.parser._
 import org.apache.spark.rdd.RDD
 import org.archive.webservices.ars.io.FileAccessContext
-import org.archive.webservices.ars.model.ArchConf
 import org.archive.webservices.ars.model.collections.inputspecs.meta.{FileMetaData, FileMetaField}
 import org.archive.webservices.sparkling.Sparkling
 import org.archive.webservices.sparkling.util.RddUtil
-
-import scala.io.Source
 
 object FileSpecLoader extends InputSpecLoader {
   val specType = "files"
@@ -106,17 +102,4 @@ object FileSpecLoader extends InputSpecLoader {
       }
     }
   }
-
-  def petaboxFiles(itemName: String): Seq[String] = {
-    val source = Source.fromURL(ArchConf.iaBaseUrl + s"/metadata/$itemName/metadata/files")
-    try {
-      parse(source.mkString).toOption
-        .flatMap(_.hcursor.values)
-        .getOrElse(Seq.empty)
-        .map(_.hcursor)
-        .flatMap { file =>
-          file.get[String]("filename").toOption.map(itemName + "/" + _)
-        }
-    } finally source.close()
-  }.toSeq
 }
