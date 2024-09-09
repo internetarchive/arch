32634440023536750be21a9c3fafefa4d693e7e0
Helge Holzmann <helge@archive.org>
2024-09-09 12:28:58 +0200

    fix: initialize addons

--- src/main/scala/org/archive/webservices/ars/util/AddonUtil.scala
+++ src/main/scala/org/archive/webservices/ars/util/AddonUtil.scala
@@ -5,11 +5,16 @@ import scala.collection.JavaConverters._
 
 object AddonUtil {
   def initializePackage(packageName: String): Unit = {
+    println(s"Initializing add-ons of package $packageName...")
     val packagePath = packageName.replace('.', '/') + "/"
     val jarEntries = ClassLoader.getSystemResources(packagePath).asScala.filter(_.getProtocol == "jar").flatMap { jar =>
       jar.getPath.stripPrefix("file:").split('!').headOption
     }.flatMap(path => new JarFile(path).entries().asScala).map(_.toString)
     val objects = jarEntries.filter(_.startsWith(packagePath)).filter(_.endsWith("$.class")).map(_.stripSuffix(".class").replace('/', '.'))
-    for (objectClass <- objects) Class.forName(objectClass)
+    for (objectClass <- objects) {
+      val c = Class.forName(objectClass)
+      println(s"Initialized add-on ${c.getCanonicalName}.")
+    }
+    println(s"Initialized add-ons of package $packageName.")
   }
 }
